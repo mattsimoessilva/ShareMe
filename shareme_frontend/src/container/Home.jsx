9ef3f9935a20ef426da406612db25383433bc854
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2022-08-02 16:10:22 -0300

    Creating Pin Component

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -8,14 +8,13 @@ import Pins from './Pins';
 import { userQuery } from '../utils/data';
 import { client } from '../client';
 import logo from '../assets/logo.png';
+import { fetchUser } from '../utils/fetchUser';
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false)
   const [user, setUser] = useState(null);
   const scrollRef = useRef(null);
-
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
-
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.sub);
@@ -32,7 +31,7 @@ const Home = () => {
 
 
   return (
-    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
       <div className='hidden md:flex h-screen flex-initial'>
         <Sidebar user={user && user} />
       </div>
