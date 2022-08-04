5014fee96e0aec4115a6ddd20bfc51515919ea44
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2022-08-03 22:03:28 -0300

    Finishing project

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -15,7 +15,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save }}) => {
 
   const user = fetchUser();
 
-  const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.sub))?.length;
+  const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user?.sub))?.length;
 
   const savePin = (id) => {
     if(!alreadySaved) {
@@ -26,10 +26,10 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save }}) => {
             .setIfMissing({ save: [] })
             .insert('after', 'save[-1]', [{
                 _key: uuidv4(),
-                userId: user.sub,
+                userId: user?.sub,
                 postedBy: {
                     _type: 'postedBy',
-                    _ref: user.sub
+                    _ref: user?.sub
                 }
             }])
             .commit()
@@ -103,7 +103,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save }}) => {
                                 <BsFillArrowUpRightCircleFill />
                             </a>
                         )}
-                        {postedBy?._id === user.sub && (
+                        {postedBy?._id === user?.sub && (
                             <button
                                 onClick={(e) => {
                                     e.stopPropagation()
